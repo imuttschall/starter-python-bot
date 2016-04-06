b4e6a3e0c0155d4715cc48c4433f1db8a45034d8
Isaac Muttschall <imuttschall@gmail.com>
2016-04-06 16:37:03 -0500

    Update rtmbot.py

--- rtmbot.py
+++ rtmbot.py
@@ -62,17 +62,20 @@ class RtmBot(object):
             if "type" in data:
                 function_name = "process_" + data["type"]
                 dbg("got {}".format(function_name))
-                print data
-
+                dbg( "data {}".format(data))
                 if "text" in data and self.isBotMention( data[ 'text' ] ):
                     function_name = "process_mention"
                 
-                if( "text" in data and self.bot_on or data[ 'text' ].startswith( "lemonbot" ) ):
+                        self.bot_on = True
+                        for plugin in self.bot_plugins:
+                            plugin.register_jobs()
+                            plugin.do(function_name, data)
+                
+                if ( "text" in data ) and ( self.bot_on or data[ 'text' ].startswith( "lemonbot" ) ):
                     
                     if( data[ "text" ] == "lemonbot shutup" ):
                         self.bot_on = False
                     
-                    else:
                     
                         self.bot_on = True
                         for plugin in self.bot_plugins:
