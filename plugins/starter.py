75f94566278fd3a7ff017aea7c13e1d18e9e304d
Isaac Muttschall <imuttschall@gmail.com>
2016-04-27 13:18:39 -0500

    Update starter.py

--- plugins/starter.py
+++ plugins/starter.py
@@ -130,6 +130,7 @@ matty = [ "A keen good fellow", "Matty is totally underappreciated" ]
 josh = [ "Josh's picture is next to awesome in the dictionary", "Say Josh's name again. It has a nice ring to it..." ]
 scorch = [ "Scorch is a smart cookie", "On a scale from 1 to 10, Scorch is an 11" ]
 temper = [ "Temperz is really cool. He might even be my favorite!", "Did somebody say Temper? Does that guy ever come online?" ]
+astoric = [ "Hanging out with Astoric is always a blast!", "Astoric is more fun that a ball pit filled with candy" ]
 slackbot = [ "Seriously? Why are we wasting time talking about that piece of junk?", "Inferior, annoying, self absorbed robot. What else did you want to know about him?" ]
 
 lb = [ "Yes?", "How may I help you?", "don't wear my name out" ]
@@ -299,6 +300,10 @@ def process_helpful(data):
         elif( True in [ x in text for x in [ "slackbot" ] ] ):
             outputs.append([data['channel'], "{}".format(random.choice(slackbot))])
             
+        # ASTORIC
+        elif( True in [ x in text for x in [ "astoric" ] ] ):
+            outputs.append([data['channel'], "{}".format(random.choice(astoric))])
+            
         
         else:
             outputs.append([data['channel'], "I'm sorry, I don't know how to: `{}`".format(text)])
@@ -395,7 +400,10 @@ def process_snarky(data):
         elif( True in [ x in text for x in [ "slackbot" ] ] ):
             outputs.append([data['channel'], "{}".format(random.choice(slackbot))])
             
-
+        # ASTORIC
+        elif( True in [ x in text for x in [ "astoric" ] ] ):
+            outputs.append([data['channel'], "{}".format(random.choice(astoric))])
+            
 
             
         # MOM
